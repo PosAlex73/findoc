e66d84138e6b81fc1ebbefaabd987747bd2e21d8
PosALex73 <alternativa988@inbox.ru>
2022-03-08 15:41:55 +0400

    add fds

--- app/Providers/ComposerProvider.php
+++ app/Providers/ComposerProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use App\BreadCrumbs\BreadCrumbs;
-use App\Composers\AdminComposers\BreadCrumbsComposer;
-use App\Composers\AdminComposers\SidebarComposer;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class ComposerProvider extends ServiceProvider
@@ -27,6 +24,6 @@ class ComposerProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('admin.*', BreadCrumbsComposer::class);
+
     }
 }
--- resources/views/admin/components/breads.blade.php
+++
@@ -1,7 +0,0 @@
-<nav class="page-breadcrumb">
-    <ol class="breadcrumb">
-        @foreach($break_crumbs as $crumb)
-            <li class="breadcrumb-item @if($loop->last) active @endif"><a href="#">{{ $crumb }}</a></li>
-        @endforeach
-    </ol>
-</nav>
