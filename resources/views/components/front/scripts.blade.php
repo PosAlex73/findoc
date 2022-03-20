ab0ec99e2cc9da12bc23bee39d7f693b4f901099
PosALex73 <alternativa988@inbox.ru>
2022-03-20 08:30:42 +0400

    add front page

--- resources/views/components/front/scripts.blade.php
+++ resources/views/components/front/scripts.blade.php
@@ -1,4 +1,4 @@
 <!-- COMMON SCRIPTS -->
-<script src="js/jquery-3.6.0.min.js"></script>
-<script src="js/common_scripts.min.js"></script>
-<script src="js/functions.js"></script>
+<script src="{{ asset('js/front/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('js/front/common_scripts.js') }}"></script>
+<script src="{{ asset('js/front/functions.js') }}"></script>
