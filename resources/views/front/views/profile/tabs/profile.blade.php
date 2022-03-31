388629589197bdad592520a243df5f3a51aa196e
PosALex73 <alternativa988@inbox.ru>
2022-03-31 08:46:49 +0400

    add additional fields

--- resources/views/front/views/profile/tabs/profile.blade.php
+++ resources/views/front/views/profile/tabs/profile.blade.php
@@ -1,5 +1,11 @@
+@php
+$user = \Illuminate\Support\Facades\Auth::user();
+@endphp
+
 <form action="{{ route('front.profile.update') }}" method="post">
     @csrf
     <p>{{ __('vars.user_profile') }}</p>
-    @include('front.fields.input', ['name' => 'first_name', 'value' => Auth::user()->first_name])
+    @include('front.fields.input', ['name' => 'first_name', 'value' => $user->first_name])
+    @include('front.fields.input', ['name' => 'last_name', 'value' => $user->last_name])
+    @include('')
 </form>
